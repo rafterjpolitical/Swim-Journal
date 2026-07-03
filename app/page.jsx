40a4028f44cb55e68c34e31f1424d23c1c1d32fb
rafterjpolitical <rafterjpolitical@gmail.com>
2026-07-03 00:07:27 -0500

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding:40,fontFamily:"system-ui",textAlign:"center"}}>
+      <h1>🏊 Swim Journal</h1>
+      <p>Coming soon</p>
+    </div>
+  )
+}
