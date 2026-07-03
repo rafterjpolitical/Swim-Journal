c62c75be8e94cb20aa7e46d6680b4df1914a2cf6
rafterjpolitical <rafterjpolitical@gmail.com>
2026-07-03 00:08:46 -0500

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = { title: "Swim Journal" }
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
