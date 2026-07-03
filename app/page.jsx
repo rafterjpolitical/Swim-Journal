3f34ceab3b7105dbe83c549528c7d9f875db10a6
rafterjpolitical <rafterjpolitical@gmail.com>
2026-07-03 15:05:52 -0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,2 @@
-export default function Home() {
-  return (
-    <div style={{padding:40,fontFamily:"system-ui",textAlign:"center"}}>
-      <h1>🏊 Swim Journal</h1>
-      <p>Coming soon</p>
-    </div>
-  )
-}
+"use client";
+export { default } from "../../SwimJournal";
